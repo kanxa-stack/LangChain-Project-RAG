b786594dfe6efb14065f78c1e4bd832802202b24
kanxa-stack <padhakukanxa@gmail.com>
2026-03-14 00:27:58 +0545

    "Embedding, Vector store setup and  storing the vector in supabase vector store"

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
+import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
+
+
+import { createClient } from '@supabase/supabase-js'
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+
+import fs from 'fs';
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import dotenv from 'dotenv'
 dotenv.config();
 
 const supaBaseAPI = process.env.SB_API_KEY;
-const supaBaseURL = process.env.SB_URL;
-console.log(supaBaseAPI);
+const supaBaseURL = process.env.SB_URI;
+
+console.log("URL0:",supaBaseAPI, supaBaseURL)
+const huggingFaceAPI = process.env.HF_API_KEY;
+
+try {
+    
+
+    const data = fs.readFileSync("scrimba-info.txt", "utf8");
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 500,
+        separators : ['\n\n', '\n',' ', ''], //by default
+        chunkOverlap: 50
+    });
+    const output = await splitter.createDocuments([data])
+
+    //Clent means creating connection to database
+    const client = createClient(supaBaseURL, supaBaseAPI);
+
+    const result = await SupabaseVectorStore.fromDocuments(
+        output,
+        new HuggingFaceInferenceEmbeddings({
+            model: "sentence-transformers/all-MiniLM-L6-v2",
+            apiKey: huggingFaceAPI, // Required for HF Inference API
+        }),
+        {
+            client: client,
+            tableName: "documents",
+        }
+        
+);
+console.log(result)
+
+} catch (error) {
+    console.warn("Error Ayo", error)
+}
